96e50bd8263a25856c8ac9037e6aaae2ae5473ed
ehsansdr <sehsan251@gmail.com>
2023-12-12 17:50:41 +0330

    uploaded 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    //hello from commit "uploaded 3"
+    //hello from commit "uploaded 5"
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) throws Exception{
         Cal cal = new Cal();
